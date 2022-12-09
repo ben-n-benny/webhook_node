ab060279ded41bf5a24b2ff933ee30cb49fc1ba3
benny <benedickbayaban.binlayo@bicol-u.edu.ph>
2022-12-09 23:48:33 +0800

    iuiugiu

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-const http = require('http')
-const port = 3000
+const express = require('express')
+const app = express()
 
-const server = http.createServer(function(req, res){
-    res.write('HELLLO')
+app.post('/',(req,res) => {
+    res.send(req)
 
-    return res.end();
-})
+    let data = JSON.parse(req)
 
-server.listen(process.env.PORT || 5000, function(error){
-    if (error){
-        console.log('Error',error);
+    if(data.attribues.type == 'source.chargeable'){
+
+        let amount = data.data.attributes.data.attributes.amount
+        let id = data.data.attributes.data.id
+        let description = "GCash Payment Description";
+
+        const sdk = require("api")("@paymongo/v1#5u9922cl2759teo");
+
+        sdk.auth("sk_test_oV5YqMuL7WmWvcGxELWavcFk");
+        sdk
+          .createAPayment({
+            data: {
+              attributes: {
+                amount: amount,
+                source: { id: id, type: "source" },
+                currency: "PHP",
+                description: description,
+              },
+              attribues: { metadata: "metadata" },
+            },
+          })
+          .then(({ data }) => console.log(data))
+          .catch((err) => console.error(err));
+
     }
     else{
-        console.log("Listening");
+
     }
+    
 })
+
+app.listen(process.env.PORT || 3000)
