9608f35b00e115bdb5e5d476c6f01d4d4e06c17f
benny <benedickbayaban.binlayo@bicol-u.edu.ph>
2022-12-10 09:24:08 +0800

    hsgsfgdf

--- app.js
+++ app.js
@@ -37,54 +37,54 @@ const db = getFirestore();
 
 app.post("/", (req, res) => {
   const request_body = req.body;
-  // console.log(req.body)
+  console.log(req.body)
   //res.send(req.body.data.attributes.type);
-  if (request_body.data.attributes.type == "source.chargeable") {
-    let amount = request_body.data.attributes.data.attributes.amount;
-    let id = request_body.data.attributes.data.id;
-    let description = "GCash Payment Description";
-    const request = require("request");
+  // if (request_body.data.attributes.type == "source.chargeable") {
+  //   let amount = request_body.data.attributes.data.attributes.amount;
+  //   let id = request_body.data.attributes.data.id;
+  //   let description = "GCash Payment Description";
+  //   const request = require("request");
 
-    const options = {
-      method: "POST",
-      url: "https://api.paymongo.com/v1/payments",
-      headers: {
-        accept: "application/json",
-        "content-type": "application/json",
-        authorization: "Basic c2tfdGVzdF9vVjVZcU11TDdXbVd2Y0d4RUxXYXZjRms6",
-      },
-      body: {
-        data: {
-          attributes: {
-            amount: amount,
-            source: { id: id, type: "source" },
-            description: description,
-            currency: "PHP",
-          },
-          attribues: { metadata: "metadata" },
-        },
-      },
-      json: true,
-    };
+  //   const options = {
+  //     method: "POST",
+  //     url: "https://api.paymongo.com/v1/payments",
+  //     headers: {
+  //       accept: "application/json",
+  //       "content-type": "application/json",
+  //       authorization: "Basic c2tfdGVzdF9vVjVZcU11TDdXbVd2Y0d4RUxXYXZjRms6",
+  //     },
+  //     body: {
+  //       data: {
+  //         attributes: {
+  //           amount: amount,
+  //           source: { id: id, type: "source" },
+  //           description: description,
+  //           currency: "PHP",
+  //         },
+  //         attribues: { metadata: "metadata" },
+  //       },
+  //     },
+  //     json: true,
+  //   };
 
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-      const res_body = body;
-      const docRef = db
-        .collection("transactions")
-        .doc(res_body.data.attributes.metadata.clientID);
+  //   request(options, function (error, response, body) {
+  //     if (error) throw new Error(error);
+  //     const res_body = body;
+  //     const docRef = db
+  //       .collection("transactions")
+  //       .doc(res_body.data.attributes.metadata.clientID);
 
-      docRef
-        .update({
-          [res_body.data.attributes.source.id+'.paid']: true,
-        })
-        .then((logging) => {
-          console.log(logging);
-        });
+  //     docRef
+  //       .update({
+  //         [res_body.data.attributes.source.id+'.paid']: true,
+  //       })
+  //       .then((logging) => {
+  //         console.log(logging);
+  //       });
 
-      res.send(body);
-    });
-  }
+  //     res.send(body);
+  //   });
+  // }
 });
 
 app.listen(process.env.PORT || 3000);
